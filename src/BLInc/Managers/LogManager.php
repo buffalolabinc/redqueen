2e10ace028e140bc061579ab6f50274ad6113c46
Matt Lehner <mlehner@gmail.com>
2015-01-15 05:54:53 +0000

    sort log files by created_at newest first

--- src/BLInc/Managers/LogManager.php
+++ src/BLInc/Managers/LogManager.php
@@ -12,7 +12,7 @@ class LogManager extends TimestampedManager
 
     protected function getFindAllQuery()
     {
-        return 'SELECT l.id, l.code, l.validPin, l.created_at, c.name FROM `logs` AS l LEFT JOIN `cards` AS c ON (l.code = c.code) GROUP BY l.id';
+        return 'SELECT l.id, l.code, l.validPin, l.created_at, c.name FROM `logs` AS l LEFT JOIN `cards` AS c ON (l.code = c.code) GROUP BY l.id ORDER BY l.created_at DESC';
     }
 
 }
